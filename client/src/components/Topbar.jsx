90b25220dd9837fb9d9d12890d232b45fdce7cc8
Tafita Hery <tafitahery@gmail.com>
2023-02-10 15:22:33 +0300

    Topbar fini

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -3,33 +3,53 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: lightblue;
   height: 50px;
   padding: 0 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  -webkit-box-shadow: 0px -1px 44px 2px rgba(0, 0, 0, 0.43);
+  box-shadow: 0px -1px 44px 2px rgba(0, 0, 0, 0.43);
 `;
 
 const Logo = styled.div`
   font-weight: 700;
-  font-size: 32px;
+  font-size: 42px;
+  cursor: pointer;
 `;
 
-const Navigation = styled.ul``;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
-const Menu = styled.li``;
+const Navigation = styled.ul`
+  list-style: none;
+  display: flex;
+  font-size: 24px;
+`;
+
+const Menu = styled.li`
+  margin-left: 20px;
+  color: lightgray;
+  font-weight: 400;
+  &:hover {
+    color: gray;
+  }
+`;
 
 export default function Topbar() {
   return (
     <Container>
-      <Logo>.lebo</Logo>
+      <Logo>
+        <StyledLink to="/">.lebo</StyledLink>
+      </Logo>
       <Navigation>
         <Menu>
-          <Link to="/">Acceuil</Link>
+          <StyledLink to="/">Acceuil</StyledLink>
         </Menu>
         <Menu>
-          <Link to="/report">Rapport</Link>
+          <StyledLink to="/report">Rapport</StyledLink>
         </Menu>
       </Navigation>
     </Container>
